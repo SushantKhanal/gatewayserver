8a1336b0f2a5960037a58b3e4cfd20aab257bd56
SushantKhanal <dreamerking114@gmail.com>
2023-09-20 23:40:50 -0500

    micrometer zipkin changes

--- src/main/java/com/eaztbytes/gatewayserver/GatewayserverApplication.java
+++ src/main/java/com/eaztbytes/gatewayserver/GatewayserverApplication.java
@@ -1,11 +1,21 @@
-package com.sushantkhanal.gatewayserver;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
+package com.eaztbytes.gatewayserver;
 
 import java.util.Date;
+
+import com.eaztbytes.gatewayserver.trace.logging.ObservationContextSnapshotLifter;
+import io.micrometer.context.ContextSnapshot;
+import io.micrometer.tracing.Tracer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.ContextRefreshedEvent;
+import reactor.core.publisher.Hooks;
+import reactor.core.publisher.Operators;
 
 @SpringBootApplication
 public class GatewayserverApplication {
@@ -13,7 +23,7 @@ public class GatewayserverApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayserverApplication.class, args);
 	}
-
+	
 	@Bean
 	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
 	    return builder.routes()
@@ -31,8 +41,16 @@ public class GatewayserverApplication {
 		            .path("/eazybank/cards/**")
 		            .filters(f -> f.rewritePath("/eazybank/cards/(?<segment>.*)","/${segment}")
 		            		.addResponseHeader("X-Response-Time",new Date().toString()))
-		            .uri("lb://CARDS"))
-				.build();
+		            .uri("lb://CARDS")).build();
+	}
+
+	@ConditionalOnClass({ContextSnapshot.class, Hooks.class})
+	@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.REACTIVE)
+	@Bean
+	public ApplicationListener<ContextRefreshedEvent> reactiveObservableHook() {
+		return event -> Hooks.onEachOperator(
+				ObservationContextSnapshotLifter.class.getSimpleName(),
+				Operators.lift(ObservationContextSnapshotLifter.lifter()));
 	}
 
 }
