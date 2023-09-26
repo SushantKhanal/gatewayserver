4f4653824b3eb21e8e9fb9575483d11f20bf8f09
SushantKhanal <dreamerking114@gmail.com>
2023-09-26 15:12:31 -0500

    security related changes in gateway server

--- src/main/java/com/eazybytes/gatewayserver/config/SecurityConfig.java
+++ src/main/java/com/eazybytes/gatewayserver/config/SecurityConfig.java
@@ -2,7 +2,9 @@ package com.eazybytes.gatewayserver.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecirity;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
@@ -10,5 +12,14 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 @EnableWebFluxSecurity
 public class SecurityConfig {
     @Bean
-    public SecurityWebFilterChain springSecurityFilterChain()
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
+        serverHttpSecurity.authorizeExchange(exchanges -> exchanges.pathMatchers(HttpMethod.GET).permitAll()
+                .pathMatchers("/eazybank/accounts/**").authenticated()
+                .pathMatchers("/eazybank/cards/**").authenticated()
+                .pathMatchers("/eazybank/loans/**").authenticated())
+                .oauth2ResourceServer(oauth2ResourceServerSpec -> oauth2ResourceServerSpec
+                        .jwt(Customizer.withDefaults()));
+        serverHttpSecurity.csrf(csrfSpec -> csrfSpec.disable());
+        return serverHttpSecurity.build();
+    }
 }
